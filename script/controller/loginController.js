edfe0b57d593d59b9f51a551949c1368ba959fc1
Igor Iwashita <igor.iwashita@aluno.ufabc.edu.br>
2019-05-17 00:52:12 -0300

    Validation is now server side

---
+++ script/controller/loginController.js
@@ -0,0 +1,66 @@
+const API_ENDPOINT = "http://gaalaxyapijs.herokuapp.com";
+
+$('#submit').click(()=>{
+
+    error = "";
+
+    if(!$('#inputEmail').val()){
+        error = "O login não pode estar vazio! <br>";
+    }
+    if(!$('#inputPassword').val()){
+        error = error + "A senha não pode estar vazia! <br>";
+    }
+    
+    if($('#inputEmail').val() && $('#inputPassword').val()){
+        $('.darken-bkg').css("display","block");
+        $('.spinner-border').show();
+
+        var user = $('#inputEmail').val();
+        var pass = $('#inputPassword').val();
+
+        const getHash = new Promise((resolve,reject)=>{
+            $.get("https://api.hashify.net/hash/md4/hex?value=" + pass,(result)=>{
+                if(result){
+                    passHash = result["Digest"];
+                    resolve(passHash);
+                }
+                else{
+                    reject("Error converting hash");
+                }
+            });
+        }).then(()=>{
+            credentials = {
+                "username":user,
+                "password":passHash
+            }
+
+            $.ajax({
+                type: "POST",
+                url: API_ENDPOINT + '/users/login',
+                data: credentials,
+                statusCode:{
+                    404:()=>{
+                        error = "Login ou senha inválidos";
+                        $("#errorMsg").html(error);
+                        $('.spinner-border').hide();
+                        $('.darken-bkg').css("display","none");
+                        
+                    },
+                    200:(response)=>{
+                        $('.spinner-border').hide();
+                        $('.darken-bkg').css("display","none");
+                        window.sessionStorage.setItem('user',user);
+                        window.sessionStorage.setItem('nome',response.nmUser);
+                        window.sessionStorage.setItem('cdUser',response.cdUser);
+                        window.sessionStorage.setItem('isLogado',true);
+                        window.location.href = "index.html";
+                    }
+                }
+            });
+        });
+    }
+
+    $("#errorMsg").html(error);
+
+
+});
